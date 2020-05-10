0f238b738df19f474a3027803ee7a75557df2e9f
Lassi Lehtinen <lokleh@utu.fi>
2020-05-10 16:50:09 +0300

    supports unicode requests

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
-# Wikitimeline
+# Timeline
 
-Application for plotting peoples lifetimes against a timeline
+## Overview
+
+Application for visualising peoples lifespans with _matplotlib_. Currently uses wikipedia as main source.
+
+timeline.py contains all main functions. _requests_ library is used to fetch html pages from sources. _BeautifulSoup_ parses dates from wikipedias "infobox" element. The results and persons name are stored in a sqlite database.
+
+Plotting is done bases on sql quaries.
+
+## TBI
+
+* Manual input
+
+## Images
+
+![timeline1](./images/timeline_01.png)
--- timeline.py
+++ timeline.py
@@ -6,12 +6,11 @@ Created on 1 Jul 2019
 @author Lassi Lehtinen
 """
 
-
 import sqlite3
 from sqlite3 import Error
 
-from urllib.request import urlopen
-from urllib.error import URLError
+import requests
+from requests.exceptions import HTTPError, ConnectionError
 
 from bs4 import BeautifulSoup
 import re
@@ -48,7 +47,7 @@ def store(name, born=None, died=None, source=None):
 def store_list(source):
     """ Start fetching dates from wikipedia with a list on names as input. Takes care of storing. """
     for person in source:
-        sleep(5)
+        #sleep(1)
         dt = []
         try:
             url = fi_wiki_url(person)
@@ -57,7 +56,6 @@ def store_list(source):
                 print('store_list: due to connection error skipping...')
                 print("--------------")
                 continue
-            print("Status code: ", uri.status)
             dt = dates(uri)
             print(dt)
 
@@ -78,39 +76,41 @@ def days_between(d1, d2):
     return abs((d2 - d1).days)
 
 def dict_factory(cursor, row):
+    """ Makes a sqlite3 quary retrun a dictionary """
     d = {}
     for idx, col in enumerate(cursor.description):
         d[col[0]] = row[idx]
     return d
 
 def get_url(url):
-    """ Get page content """
+    """ Get page content as a string of html """
     print(url)
     try:
-        html = urlopen(url, timeout = 10)
-    except URLError as e:
+        html = requests.get(url, timeout=5, allow_redirects=True)
+        print('success %r' % html)
+    except (HTTPError, ConnectionError) as e:
         print("get_url: ", e)
         html = None
-    return html
+    return html.text
 
 def fi_wiki_url(name):
-    """ Attach finnish wikipedia url """
+    """ Create a finnish wikipedia page url from a persons name """
     name = name.replace(' ', '_')
     return 'https://fi.wikipedia.org/wiki/'+name
 
 def en_wiki_url(name):
-    """ Attach english wikipedia url """
+    """ Create an english wikipedia page url from a persons name """
     name = name.replace(' ', '_')
     return 'https://en.wikipedia.org/wiki/'+name
 
 def print_persons(input_list):
-    """ Print information on command line """
+    """ Try to find people based on a list of names. Prints results on the command line """
     for person in input_list:
         sleep(5)
         print_infobox(person)
 
 def dates(html):
-    """ Primary way of finding dates """
+    """ Primary way of finding dates. Takes the html """
     dates = []
     bsObj = BeautifulSoup(html, 'lxml')
     infobox = bsObj.find("table", {"class": "infobox"})
@@ -125,7 +125,7 @@ def dates(html):
         print('dates: infobox not found')
 
 def dates_backup(html):
-    """ Secondary way of finding dates """
+    """ Secondary way of finding dates. Takes raw html """
     dates = []
     bsObj = BeautifulSoup(html, 'lxml')
     paragraph = bsObj.find("div", {"id": "mw-content-text"})
@@ -139,11 +139,43 @@ def dates_backup(html):
             except:
                 print("Kelvoton pvm...")
         return dates
+    if dates is not None:
+        return dates
     else:
-        return None
+        print("dates_backup: no luck")
+
+def print_by_age():
+    get = """
+    SELECT * FROM people
+    """
+    try:
+        con = sqlite3.connect(db_loc)
+        con.row_factory = dict_factory
+        cur = con.cursor()
+        cur.execute(get)
+    except Error as e:
+        print("print_by_age: ", e)
+
+    source = cur.fetchall()
+    con.close()
+
+    source = add_dates(source)
+    for i in source:
+        age = days_between(i['bday'],i['dday'])
+        i['age'] = age
+
+    print("{:<12} {:<12} {:<10}".format('Last name', 'First name', 'Age'))
+    for life in sorted(source, key = lambda i: i['age']):
+        year_format = to_years(life['age'])
+        print("{:<12} {:<12} {:<10}".format(life['lname'], life['fname'], year_format))
+        #print("{:<12} {:<12} {:<10}".format(life['lname'], life['fname'], life['age']))
+
+def to_years(days):
+    """ Convert days to approximate years and days """
+    return "{} y {} d".format(int(days/365), days % 365)
 
 def print_infobox(name):
-    """ Fetch information from infobox element """
+    """ Fetch birthday and passing dates """
     print(name.upper())
     html = get_url(fi_wiki_url(name))
     if html is not None:
@@ -162,7 +194,6 @@ def print_infobox(name):
                 print("{} days: {} years and {} days".format(age, int(age/365), age % 365))
             else:
                 print("Nothing found...")
-    print("--------")
 
 def convertDate(date):
     """ Format finnish month names to numbers """
@@ -182,7 +213,7 @@ def convertDate(date):
     return newdate
 
 def datestr2numlist(date):
-    """ Takes a date in string format and separated them to int list  """
+    """ Takes a date in string format and separated them to int list """
     newdate = date.split('.')
     intlist = []
     for i in newdate:
@@ -190,22 +221,47 @@ def datestr2numlist(date):
     return intlist
 
 def plot_persons(amount):
-    """ Plots timelines from database TO BE MOVED"""
+    """ 
+    Plots lifetime timelines from database to a matlib plot.
+    Fetches all data from sqlite database and takes a random sample basen on param amount.
+    """
     try:
         con = sqlite3.connect(db_loc)
         con.row_factory = dict_factory
         cur = con.cursor()
-        cur.execute("SELECT fname, lname, bday, dday FROM people")
+        cur.execute("SELECT fname, lname, bday, dday FROM people WHERE bday LIKE \'19%\'")
     except Error as e:
         print("plot_persons: ", e)
 
-    fig, ax = plt.subplots(1)
+    fig, ax = plt.subplots(figsize=(10,6))
+    fig.tight_layout()
 
     index = 1
     source = cur.fetchall()
+    con.close()
+    
     people = sample(source, amount)
+    people = add_dates(people)
 
-    for person in people:
+    for life in sorted(people, key = lambda i: i['bday']):
+        name = '{} {}'.format(life['fname'], life['lname'])
+        plt.plot([life['bday'], life['dday']], [index, index], linewidth=4, label=name)
+        index += 1
+
+    plt.ylim(bottom=0, top=amount+1)
+    plt.xlim(right=d.today())
+    ax.legend()
+    ax.xaxis.set_major_locator(years)
+    ax.xaxis.set_minor_locator(years_minor)
+    fig.autofmt_xdate()
+    plt.gca().axes.get_yaxis().set_visible(False)
+    #plt.tight_layout(pad=0.5, h_pad=0.5, w_pad=0.5)
+    plt.grid(True)
+    plt.savefig('./images/timeline_01.png', dpi=100)
+
+def add_dates(input_dict):
+    """ Change dict string dates to datetime objects """
+    for person in input_dict:
         person['bday'] = datetime.datetime.strptime(person['bday'], '%Y-%m-%d').date()
         if ( person['dday'] == None ):
             person['dday'] = d.today()
@@ -213,37 +269,25 @@ def plot_persons(amount):
             person['dday'] = d.today()
         elif (isinstance(person['dday'], str)):
             person['dday'] = datetime.datetime.strptime(person['dday'], '%Y-%m-%d').date()
-
-    for life in people:
-        name = '{} {}'.format(life['fname'], life['lname'])
-        plt.plot([life['bday'], life['dday']], [index, index], linewidth=4, label=name)
-        index += 1
-
-    plt.ylim(bottom=0, top=amount+1)
-    ax.legend()
-    ax.xaxis.set_major_locator(years)
-    ax.xaxis.set_minor_locator(years_minor)
-    fig.autofmt_xdate()
-    plt.gca().axes.get_yaxis().set_visible(False)
-    plt.tight_layout(pad=0.5, h_pad=0.5, w_pad=0.5)
-    plt.grid(True)
-    plt.show()
-    con.close()
+    return input_dict
 
 def user_input():
+    """ Interactive way of inputting people data to database """
     person = ''
     print("Input people to database:")
     while person is not 'quit':
         person = input('Name: ')
         if person == 'quit':   
              break
+        elif person == 'print':
+            count = input('How many:')
+            plot_persons(int(count))
+            continue
+        elif person == 'age':
+            print_by_age()
+            continue
         store_list([person])
     print("Exiting...")
 
 if __name__ == '__main__':
-    #print_persons(sample(randoms, 5))
-    plot_persons(20)
-    #store_list(sample(randoms, 20))
-    #finnish = ['Aku Hirviniemi', 'Jaakko Saariluoma', 'Jonne Aaron', 'Mauno Koivisto', 'Urho Kekkonen', 'Risto Ryti', 'Tuomas Holopainen', 'Jenni Vartiainen']
-    #store_list(finnish)
-    #user_input()
+    user_input()
